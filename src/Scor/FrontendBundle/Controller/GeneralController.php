ea7fba80240b3702109e1b502c7baf3912ff0f97
Dani Sancas <lord.sancas@gmail.com>
2014-05-29 00:16:59 +0200

    Añadimos sección de localización y contacto, con un mapa y un formulario - Creamos CommonBundle/Form/ContactoType y eliminamos Acme/DemoBundle - Ajustamos la configuración necesaria - Añadimos estilos necesarios (normales y responsive) - Creamos una plantilla alternativa de formulario para ser llamada mediante {% form_theme ... %} en TWIG - Añadimos plantilla para flashes - Colocamos en un fichero a parte el header (menú) del layout de FrontendBundle - Añadimos la extensión ControllerActionExtension, como suele ser habitual
    TODO
    - Añadir sección y páginas de permisos y licencias
    - Estudiar hacer un aviso legal o términos y condiciones de uso
    - Diseñar pie de página
    
    Signed-off-by: Dani Sancas <lord.sancas@gmail.com>

--- src/Scor/FrontendBundle/Controller/GeneralController.php
+++ src/Scor/FrontendBundle/Controller/GeneralController.php
@@ -5,6 +5,8 @@ namespace Scor\FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Scor\CommonBundle\Form\ContactoType;
+
 class GeneralController extends Controller
 {
     /**
@@ -15,5 +17,50 @@ class GeneralController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * Acción que muestra y procesa el formulario de contacto.
+     *
+     * @Template()
+     */
+    public function contactoAction()
+    {
+        $form = $this->createForm(new ContactoType());
+
+        $request = $this->get('request');
+
+        if ($request->isMethod('POST'))
+        {
+            $form->submit($request);
+
+            if ($form->isValid())
+            {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('['.$this->container->getParameter('dominio').'] Consulta desde la web')
+                    ->setFrom($form->get('email')->getData())
+                    ->setTo($this->container->getParameter('contacto_email'))
+                    ->setBody(
+                        $this->renderView(
+                            'FrontendBundle:General:emailContacto.txt.twig',
+                            array(
+                                'nombre' => $form->get('nombre')->getData(),
+                                'apellidos' => $form->get('apellidos')->getData(),
+                                'email' => $form->get('email')->getData(),
+                                'telefono' => $form->get('telefono')->getData(),
+                                'consulta' => $form->get('consulta')->getData()
+                            )
+                        )
+                    );
+
+                $this->get('mailer')->send($message);
+
+                $request->getSession()->getFlashBag()->add('ok', 'Se ha enviado su email. Gracias por contactar con nosotros.');
+
+                return $this->redirect($this->generateUrl('contacto'));
+            }
+        }
+
+        return array('form' => $form->createView(), 'contactoEmail' => $this->container->getParameter('contacto_email'));
     }
 }
